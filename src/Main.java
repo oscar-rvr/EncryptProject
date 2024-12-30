683bcccabd2e63064d6edefc8f9e9f7431b38d2e
Oscar Vega <oscar_rvr@outlook.com>
2024-12-30 17:30:40 -0600

    fase 5 completada desde mac

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,77 @@
+import java.io.*;
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-//Original message
-        Scanner scan = new Scanner(System.in);
-        String target="";
-        String message="";
-        int key;
+//valores predeterminados
+        String mode = "enc";
+        int key = 0;
+        String data = "";
+        String outFile=null;
+        //Revisar los argumentos del  arreglo args[]
+
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].equals("-mode")) {
+                mode = args[i + 1];//Si  encontramos -mode el siguiente argumento es el modo
+            } else if (args[i].equals("-key")) {
+                try {
+                    key = Integer.parseInt(args[i + 1]);
 
 
-        while(true){
-            target=scan.nextLine();
+                } catch (NumberFormatException e) {
+                    System.out.println("Error: key value must be a number");
+                    return;
+                }
+            } else if (args[i].equals("-data")) {
+                data = args[i + 1];
+            }else if(args[i].equals("-in") && data.isEmpty()){
+                //espeeramos leer el archivo
+                data=readFromFile(args[i+1]);
+                System.out.println(data);
+                if(data  ==  null){
+                    System.out.println("Error; input file does not exist");
+                    return;
 
-            if(target.equals("enc") || target.equals("dec")){
+                }
+            }else if(args[i].equals("-out")){
+                outFile=args[i+1];
+            }
+        }
+        if(data.isEmpty() && outFile == null){
+            System.out.println("Error; no input data provided or filename");
+            return;
+        }
 
-                message = scan.nextLine();
-                key = scan.nextInt();
-                scan.nextLine();
-                //Stringbuilder for create cipherText;
+
+
+
+
+
+
+
+
+            if(mode.equals("enc") || mode.equals("dec")){
+
+
+
                 StringBuilder ciphertext =   new StringBuilder();
-                ciphertext=processMessage(message,key, target.equals("enc"));
+                ciphertext=processMessage(data,key, mode.equals("enc"));
 
 
-                System.out.println(ciphertext);
+
+                if(outFile!=null){
+                    writeToFile(outFile,ciphertext.toString());
+                }else{
+                    System.out.println(ciphertext);
+                }
             }
 
 
 
-        }
+
+
 
 
 
@@ -43,7 +85,7 @@ public class Main {
     }
 
     private static char funDecryptLetter(char letter, int key){
-        return (char) ('a' +(letter  -  'a' - key)%26);
+        return (char) ('a' +(letter  -  'a' - key + 26 )%26);
     }
     private static boolean isLowerCase(char c){
         return c>= 'a' && c<='z';
@@ -68,4 +110,27 @@ public class Main {
         return resultText;
     }
 
+    private static String readFromFile(String filename){
+        try{
+            BufferedReader reader = new BufferedReader(new FileReader(filename));
+            StringBuilder content = new StringBuilder();
+            String line;
+            while((line =reader.readLine()) != null){
+                content.append(line).append("\n");
+            }
+            reader.close();
+            return content.toString();
+        }catch (IOException e){
+            return null;
+        }
+    }
+
+    private static void writeToFile(String filename ,String content){
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter(filename))){
+            writer.write(content);
+        } catch (IOException e) {
+            System.out.println("Error: unable to write to file-> "+filename);
+        }
+    }
 }
+
