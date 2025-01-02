820d643c34148a9ab63b4e57da7cddce5479f091
Oscar Vega <oscar_rvr@outlook.com>
2025-01-02 14:26:25 -0600

    fase3 completed for hyperskil

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,30 @@
+
+import java.util.Scanner;
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-//Original message
+
         Scanner scan = new Scanner(System.in);
-        String target="";
-        String message="";
+        String target = "";
+        String message = "";
         int key;
 
 
-        while(true){
-            target=scan.nextLine();
 
-            if(target.equals("enc") || target.equals("dec")){
-
-                message = scan.nextLine();
-                key = scan.nextInt();
-                scan.nextLine();
-                //Stringbuilder for create cipherText;
-                StringBuilder ciphertext =   new StringBuilder();
-                ciphertext=processMessage(message,key, target.equals("enc"));
-
-
-                System.out.println(ciphertext);
-            }
+        target = scan.nextLine();
 
 
 
-        }
+        message = scan.nextLine();
+        key = scan.nextInt();
+        scan.nextLine();
 
+        StringBuilder ciphertext = new StringBuilder();
+        ciphertext = processMessage(message, key, target.equals("enc"));
+
+
+        System.out.println(ciphertext);
 
 
 
@@ -38,34 +32,3 @@ public class Main {
 
 
     }
-    private static char funEncryptLetter ( char letter,int key){
-        return (char) ('a' + (letter - 'a' + key)%26);
-    }
-
-    private static char funDecryptLetter(char letter, int key){
-        return (char) ('a' +(letter  -  'a' - key)%26);
-    }
-    private static boolean isLowerCase(char c){
-        return c>= 'a' && c<='z';
-    }
-
-    private static StringBuilder processMessage(String message, int key, boolean isEncrypt){
-        StringBuilder resultText = new StringBuilder();
-        for(int i = 0; i<message.length(); i++){
-            char c = message.charAt(i);
-            if(isLowerCase(c)){
-                char proccessedChar;
-                        if(isEncrypt){
-                            proccessedChar=funEncryptLetter(c,key);
-                        }else{
-                            proccessedChar=funDecryptLetter(c,key);
-                        }
-                        resultText.append(proccessedChar);
-            }else{
-                resultText.append(c);
-            }
-        }
-        return resultText;
-    }
-
-}
